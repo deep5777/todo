531ec3a2ff5d378054a0b0c728a24567c8dd74bc
deep5777 <deepgalani126@gmail.com>
2024-03-09 12:04:34 +0530

    simple Todo App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Todo app
+
+this application contains a simple todo application it has following features -
+
+- Anyone can create Todo
+- Anyone can see their existing Todo
+- Anyone can mark their Todo
---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const experss = require("express");
+const app = experss();
+
+app.use(experss.json());
+
+
+app.get('/todo', function (req, res) {
+
+})
+
+app.post('/todo', function (req, res) {
+
+})
+
+app.put("/completed", function (req, res) {
+
+})
