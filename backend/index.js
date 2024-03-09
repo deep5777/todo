208cdf00a18ba8c476ef0a463ad7bf05fb668a73
deep5777 <deepgalani126@gmail.com>
2024-03-09 12:38:17 +0530

    new index file

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,42 @@
 const experss = require("express");
 const app = experss();
+const { createTodo, updateTodo } = require("./types");
+
 
 app.use(experss.json());
 
 
 app.get('/todo', function (req, res) {
 
+
+
 })
 
 app.post('/todo', function (req, res) {
+  const createPayload = req.body;
+  const parsePayload = createTodo.safeParse(createPayload);
+
+  if (!parsePayload.success) {
+    res.status(403).json({
+      msg: "You sent wrong inputs"
+    })
+
+    return;
+  }
+
 
 })
 
 app.put("/completed", function (req, res) {
+  const updatePaylod = req.body;
+  const parsePayload = updateTodo.safeParse(updatePaylod);
+
+  if (!parsePayload.success) {
+    res.status(403).json({
+      msg: "You sent wrong inputs"
+    })
+
+    return;
+  }
 
 })
